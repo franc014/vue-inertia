66bd6ba0af5f341d6f92f821184426b496d87dfb
Francisco Andrade <jfandradea@gmail.com>
2025-07-01 14:12:49 -0500

    trying mocking apis with msw lib

---
+++ resources/js/tests/setup_files/mock_server_calls.ts
@@ -0,0 +1,29 @@
+import { afterAll, afterEach, beforeAll } from 'vitest';
+import { setupServer } from 'msw/node';
+import { http, HttpResponse } from 'msw';
+
+
+const cart =
+{
+    ui_cart_id: 1,
+    items: []
+};
+
+
+
+export const restHandlers = [
+    http.post('http://vue-inertia.test/cart/create', () => {
+        return HttpResponse.json(cart);
+    }),
+];
+
+const server = setupServer(...restHandlers);
+
+// Start server before all tests
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+
+// Close server after all tests
+afterAll(() => server.close());
+
+// Reset handlers after each test for test isolation
+afterEach(() => server.resetHandlers());
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         globals: true,
+        setupFiles: ['./resources/js/tests/setup_files/mock_server_calls.ts'],
     },
 });
